d7f81f7bf97b08e10dc8486a5737f79c7e115b3d
avnrz <40942474+avnrz@users.noreply.github.com>
2020-09-21 18:43:01 +1000

    contact method

--- contact.js
+++ contact.js
@@ -17,3 +17,35 @@ document.addEventListener('input', function(event) {
   if (event.target.tagName.toLowerCase() !== 'textarea') return;
   autoExpand(event.target);
 }, false);
+
+
+// contact submission
+
+
+$('.contact-form').on('submit',function(e){
+        //optional validation code here
+  
+        e.preventDefault();
+      
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbxuSCQS7maeU-2N3E7T0fgqGlby5bGOEmLSvmh2/exec",
+            method: "POST",
+            dataType: "json",
+            data: $(".contact-form").serialize(),
+            success: function(response) {
+                
+                if(response.result == "success") {
+                    $('.contact-form')[0].reset();
+                    alert('Thank you for contacting us.');
+                    return true;
+                }
+                else {
+                    alert("Something went wrong. Please try again.")
+                }
+            },
+            error: function() {
+                
+                alert("Something went wrong. Please try again.")
+            }
+        })
+    });
