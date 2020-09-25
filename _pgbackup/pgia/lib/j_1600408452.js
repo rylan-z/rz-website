d9168c9133cefc55c9d504131b2e11c9e31853ea
avnrz <40942474+avnrz@users.noreply.github.com>
2020-09-25 14:23:51 +1000

    1

--- _pgbackup/pgia/lib/j_1600408452.js
+++
@@ -1,8 +0,0 @@
-document.onreadystatechange = function(){
-  if(document.readyState == "complete"){
-    var maxLines = 10,
-        textarea = document.getElementById('demo');
-
-    var autoGrowTextarea = new Autogrow(textarea, maxLines);
-  }
-}
--- expanding.js
+++ expanding.js
@@ -17,3 +17,10 @@ document.addEventListener('input', function(event) {
   if (event.target.tagName.toLowerCase() !== 'textarea') return;
   autoExpand(event.target);
 }, false);
+
+var form = document.querySelector('.pageclip-form')
+Pageclip.form(form, {
+  onSubmit: function (event) { },
+  onResponse: function (error, response) { },
+  successTemplate: '<span>Thank you! Your submission has been received, I will respond shortly</span>'
+})
